4bf8e52464c330ec763df15edd0c8e6a421f02d7
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2013-06-09 01:10:13 -0300

    Add more tests and test signatures to be implemented

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
 #encoding:utf-8
 
+import pytest
 import requests
 import commands
 from tempfile import NamedTemporaryFile
@@ -15,4 +16,20 @@ def test_request_aac_should_demux_from_ts():
     assert "Advanced Audio Codec" in mediainfo_output and "Video" not in mediainfo_output
     assert os.path.exists(aac_file.name)
 
+def test_request_aac_should_return_audio_aac_on_mime_type():
+    response = requests.get("http://localhost:8080/tvg.aac")
+    assert "audio/aac" == response.headers['content-type']
+
+def test_request_aac_from_invalid_ts_should_return_40x():
+    response = requests.get("http://localhost:8080/this_chunk_doesnt_exist.aac")
+    assert 404 == response.status_code
+    # improvement (issue #18)
+    #assert "text/plain" == response.headers['content-type']
+    #assert "the ts associated to aac requested does not exist" == response.text
+
+def test_request_aac_from_ts_without_aac_should_return_40x():
+    pytest.skip("Not implemented yet. Issue #19")
+
+def test_request_aac_from_ts_with_mp3_should_return_500():
+    pytest.skip("Not implemented yet. Issue #20")
 
