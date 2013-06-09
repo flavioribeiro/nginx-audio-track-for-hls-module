806b5610deb86b3268bff8a8ac9c7c64b09f3d13
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2013-06-09 01:12:10 -0300

    Avoid yoda conditions/conventions

--- tests/tests.py
+++ tests/tests.py
@@ -12,17 +12,17 @@ def test_request_aac_should_demux_from_ts():
     aac_file.write(response.content)
     mediainfo_output = commands.getoutput("mediainfo " + aac_file.name)
 
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert "Advanced Audio Codec" in mediainfo_output and "Video" not in mediainfo_output
     assert os.path.exists(aac_file.name)
 
 def test_request_aac_should_return_audio_aac_on_mime_type():
     response = requests.get("http://localhost:8080/tvg.aac")
-    assert "audio/aac" == response.headers['content-type']
+    assert response.headers['content-type'] == "audio/aac"
 
 def test_request_aac_from_invalid_ts_should_return_40x():
     response = requests.get("http://localhost:8080/this_chunk_doesnt_exist.aac")
-    assert 404 == response.status_code
+    assert response.status_code == 404
     # improvement (issue #18)
     #assert "text/plain" == response.headers['content-type']
     #assert "the ts associated to aac requested does not exist" == response.text
